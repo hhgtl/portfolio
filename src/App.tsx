a262c02f7b5f6a266d03b7c517ea949a3fdf8eec
Petro Sahal <petrosahal66@gmail.com>
2025-08-02 02:25:54 +0300

    feat: add particles

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,104 @@ import { Skills } from './layout/section/skills/Skills';
 import { Projects } from './layout/section/projects/Projects';
 import { Contact } from './layout/section/contact/Contact';
 import { Footer } from './layout/footer/Footer';
+import {useEffect, useState} from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim";
 
 function App() {
+    const [ init, setInit ] = useState(false);
+
+    useEffect(() => {
+        initParticlesEngine(async (engine) => {
+            await loadSlim(engine);
+        }).then(() => {
+            setInit(true);
+        });
+    }, []);
+
   return (
     <div className="App">
-      <Header />
-      <Main />
-      <Skills />
-      <Projects />
-      <Contact />
-      <Footer />
+        <div className="ParticlesWrapper">
+            { init && <Particles
+                id="tsparticles"
+                options={{
+                    background: {
+                        color: {
+                            value: "#191919",
+                        },
+                    },
+                    fpsLimit: 120,
+                    interactivity: {
+                        events: {
+                            onClick: {
+                                enable: true,
+                                mode: "push",
+                            },
+                            onHover: {
+                                enable: true,
+                                mode: "repulse",
+                            },
+                        },
+                        modes: {
+                            push: {
+                                quantity: 4,
+                            },
+                            repulse: {
+                                distance: 200,
+                                duration: 0.4,
+                            },
+                        },
+                    },
+                    particles: {
+                        color: {
+                            value: "#ffffff",
+                        },
+                        links: {
+                            color: "#ffffff",
+                            distance: 150,
+                            enable: true,
+                            opacity: 0.5,
+                            width: 1,
+                        },
+                        move: {
+                            direction: "none",
+                            enable: true,
+                            outModes: {
+                                default: "bounce",
+                            },
+                            random: false,
+                            speed: 6,
+                            straight: false,
+                        },
+                        number: {
+                            density: {
+                                enable: true,
+                            },
+                            value: 80,
+                        },
+                        opacity: {
+                            value: 0.5,
+                        },
+                        shape: {
+                            type: "circle",
+                        },
+                        size: {
+                            value: { min: 1, max: 5 },
+                        },
+                    },
+                    detectRetina: true,
+                }}
+            />
+            }
+        </div>
+        <div>
+          <Header />
+          <Main />
+          <Skills />
+          <Projects />
+          <Contact />
+          <Footer />
+      </div>
     </div>
   );
 }
